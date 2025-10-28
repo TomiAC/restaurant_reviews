dcddd2b1f60922a8c6e7684a969f167166d620b9
TommiC <tomicar200894@gmail.com>
2025-10-28 11:28:26 -0300

    Update Redis client connection to use service hostname

--- app/redis_manager.py
+++ app/redis_manager.py
@@ -1,6 +1,6 @@
 import redis
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 if __name__ == "__main__":
     try:
