a5102d458405ee5b61ef6bf3153bb710a3df75b7
TommiC <tomicar200894@gmail.com>
2025-10-28 11:35:34 -0300

    Refactor Redis client initialization to support environment variable for connection URL

--- app/redis_manager.py
+++ app/redis_manager.py
@@ -1,6 +1,12 @@
 import redis
+import os
 
-redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+redis_url = os.environ.get("REDIS_URL")
+
+if redis_url:
+    redis_client = redis.from_url(redis_url, decode_responses=True)
+else:
+    redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 if __name__ == "__main__":
     try:
